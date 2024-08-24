301cf8d6cc1fbfd770a81712ad72f44b4c84a37a
Elsayed Moawad <smoawad66@gmail.com>
2024-08-24 06:19:47 +0300

    recipe slayer semi-final

--- app/src/main/java/com/example/recipeslayer/ui/recipe/fragments/IdeasFragment.kt
+++ app/src/main/java/com/example/recipeslayer/ui/recipe/fragments/IdeasFragment.kt
@@ -6,18 +6,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.widget.AppCompatButton
 import androidx.lifecycle.lifecycleScope
 import com.example.recipeslayer.R
+import com.example.recipeslayer.utils.Config
+import com.example.recipeslayer.utils.Constants
 import com.example.recipeslayer.utils.Constants.GEMINI_API_KEY
 import com.google.ai.client.generativeai.GenerativeModel
 import com.google.ai.client.generativeai.type.GenerateContentResponse
 import com.google.android.material.textfield.TextInputEditText
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.Dispatchers.Main
-import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
@@ -47,9 +47,12 @@ class IdeasFragment : Fragment() {
                 modelName = "gemini-1.5-flash",
                 apiKey = GEMINI_API_KEY
             )
-            val prompt = "I am using you for my android app. I am using you to answer questions in the context of cooking. The user will give you a prompt. If it is out of the context of cooking, just answer that you only know about cooking. Give me the response in plain text with no formatting (no stars '*'). Here is the prompt: ${promptEt.text.toString()}"
+            var prompt = Constants.englishPrompt
+            if(Config.isArabic()) {
+                prompt = Constants.arabicPrompt
+            }
             lifecycleScope.launch(IO) {
-                response = generativeModel.generateContent(prompt)
+                response = generativeModel.generateContent(prompt + promptEt.text.toString())
                 withContext(Main){ responseTv.text = response.text }
                 Log.i("lol", "onCreate: ________________${response.text}")
             }
--- app/src/main/java/com/example/recipeslayer/utils/Constants.kt
+++ app/src/main/java/com/example/recipeslayer/utils/Constants.kt
@@ -34,6 +34,10 @@ object Constants {
         "معيز"
     )
 
+    const val arabicPrompt = " أنا أستخدمك لتطبيق الأندرويد الخاص بي. أنا أستخدمك للإجابة على الأسئلة في سياق الطبخ. سيعطيك المستخدم موجهًا. إذا كان خارج سياق الطبخ، فقط أجب بأنك تعرف فقط عن الطبخ. ها هو الموجه: "
+    const val englishPrompt = "I am using you for my android app. I am using you to answer questions in the context of cooking. The user will give you a prompt. If it is out of the context of cooking, just answer that you only know about cooking. Here is the prompt: "
+
+
     const val BASE_INGREDIENT_URL = "https://www.themealdb.com/images/ingredients"
     const val GEMINI_API_KEY = "AIzaSyBJDwCxczi8DQa6LY5ig0SZNOO-dUIyoYM"
 
